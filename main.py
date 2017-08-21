d4fbd822fbdc6df260a10404e08ae184e87ded26
Anthony Tournier <anthony.tournier25@gmail.com>
2017-08-22 00:01:13 +0200

    Fix & constant var

--- main.py
+++ main.py
@@ -9,28 +9,25 @@ __author__ = "Anthony Tournier"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-config = {}
+__config__ = {}
 with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
+    __config__ = json.load(json_data_file)
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    def do_POST(self):        
-        request_path = self.path
+    def do_POST(self):
         request_headers = self.headers
         content_length = request_headers.getheaders('content-length')
         length = int(content_length[0]) if content_length else 0
         data = self.rfile.read(length)
         logger.info('Payload received' + data)
-        # self.send_response(200)
     do_PUT = do_POST
 
 def main():
-    """ Maint input of app """
     logger.info('DATAS')
-    port = int(config['csgogsi']['port'])
-    logger.info('Listening on ' + config['csgogsi']['ip'] + ':' + config['csgogsi']['port'])
-    server = HTTPServer((config['csgogsi']['ip'], port), RequestHandler)
+    port = int(__config__['csgogsi']['port'])
+    logger.info('Listening on ' + __config__['csgogsi']['ip'] + ':' + __config__['csgogsi']['port'])
+    server = HTTPServer((__config__['csgogsi']['ip'], port), RequestHandler)
     server.serve_forever()
 
 if __name__ == "__main__":
