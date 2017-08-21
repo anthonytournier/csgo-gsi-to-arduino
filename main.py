4fd9ba141556169c7733c99338d9888076c53dcb
Anthony Tournier <anthony.tournier25@gmail.com>
2017-08-21 23:59:02 +0200

    Adding CSGO listenner HTTP Server

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import json
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from logzero import logger
+import json
+
 
 __author__ = "Anthony Tournier"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-__conf__ = {}
+config = {}
 with open('config.json') as json_data_file:
-    __conf__ = json.load(json_data_file)
+    config = json.load(json_data_file)
 
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):        
+        request_path = self.path
+        request_headers = self.headers
+        content_length = request_headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        data = self.rfile.read(length)
+        logger.info('Payload received' + data)
+        # self.send_response(200)
+    do_PUT = do_POST
 
 def main():
     """ Maint input of app """
-    logger.info("TODO")
+    logger.info('DATAS')
+    port = int(config['csgogsi']['port'])
+    logger.info('Listening on ' + config['csgogsi']['ip'] + ':' + config['csgogsi']['port'])
+    server = HTTPServer((config['csgogsi']['ip'], port), RequestHandler)
+    server.serve_forever()
 
 if __name__ == "__main__":
     main()
