cf52744a2ce0b523eb083a0474a196ac01134c9f
Anthony Tournier <hello@anthonytournier.fr>
2017-08-24 22:01:00 +0200

    Json decode's test in Arduino

---
+++ arduino/sketch.ino
@@ -0,0 +1,41 @@
+#include <ArduinoJson.h>
+ 
+#define REDPIN 5
+#define GREENPIN 6
+#define BLUEPIN 3
+#define BUFFER_SIZE 500
+#define BAUD_RATE 9600
+
+void setup() {
+	initSerial();
+	pinMode(REDPIN, OUTPUT);
+	pinMode(GREENPIN, OUTPUT);
+	pinMode(BLUEPIN, OUTPUT);
+}
+
+void loop() {
+	StaticJsonBuffer<200> jsonBuffer;
+
+	char json[] = "{\"round\": {\"phase\": \"live\",\"bomb\": \"planted\"}}";
+	JsonObject& root = jsonBuffer.parseObject(json);
+	
+	if (!root.success()) {
+		Serial.println("parseObject() failed");
+		return;
+	}
+
+	const char* bombStatus = root["round"]["bomb"];
+	if(bombStatus == "planted") {
+
+	}
+	
+	Serial.println(bombStatus);
+}
+
+void initSerial() {
+	Serial.begin(BAUD_RATE);
+	while (!Serial) {
+	  ; 
+	}
+	Serial.println("Serial ready");
+}
